57a6e53781b60995ab86761cbd200deab9cd7862
RautelaSahil <sahil.rautela.ssr@gmail.com>
2026-01-28 22:08:05 +0530

    Add models, migrations, and dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 #read config from Config class in config.py
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 # import after creation of app so later when we need to call app in routes it don't cause circular imports.
-from app import routes
+from app import routes, models
